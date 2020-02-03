0a12f26f889f17ef1c6377a55489f6be94627eff
sunlggggg <sunlggggg@gmail.com>
2020-02-03 13:04:59 +0800

    feat: list

---
+++ dir.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const testFolder = './node_modules/';
+const fs = require('fs');
+
+function getFiles(dir, files_) {
+  files_ = files_ || [];
+  var files = fs.readdirSync(dir);
+  for (var i in files){
+      var name = dir + '/' + files[i];
+      if (fs.statSync(name).isDirectory()){
+          getFiles(name, files_);
+      } else {
+          files_.push(name);
+      }
+  }
+  return files_;
+}
+console.log(getFiles(testFolder))
